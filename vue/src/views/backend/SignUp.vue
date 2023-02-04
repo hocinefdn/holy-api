4198b558c69189300630a8d2e65fe419b5499320
hocinefdn <hocinefedani@gmail.com>
2023-02-04 20:17:29 +0100

    Structurate admin frontend

--- vue/src/views/backend/SignUp.vue
+++ vue/src/views/backend/SignUp.vue
@@ -1,26 +1,4 @@
-<!--
-  This example requires some changes to your config:
-
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
 <template>
-    <!--
-      This example requires updating your template:
-
-      ```
-      <html class="h-full bg-gray-50">
-      <body class="h-full">
-      ```
-    -->
     <div
         class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
     >
@@ -41,6 +19,28 @@
                 <input type="hidden" name="remember" value="true" />
                 <div class="-space-y-px rounded-md shadow-sm">
                     <div>
+                        <label for="lastname" class="sr-only">Nom</label>
+                        <input
+                            id="lastname"
+                            name="lastname"
+                            type="text"
+                            required=""
+                            class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Nom"
+                        />
+                    </div>
+                    <div>
+                        <label for="firstname" class="sr-only">Prénom</label>
+                        <input
+                            id="firstname"
+                            name="firstname"
+                            type="text"
+                            required=""
+                            class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Prénom"
+                        />
+                    </div>
+                    <div>
                         <label for="email-address" class="sr-only"
                             >Adresse email</label
                         >
@@ -54,6 +54,18 @@
                             placeholder="Adresse email"
                         />
                     </div>
+                    <div>
+                        <label for="phone" class="sr-only">Téléphone</label>
+                        <input
+                            id="phone"
+                            name="phone"
+                            type="phone"
+                            required=""
+                            class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Téléphone"
+                        />
+                    </div>
+
                     <div>
                         <label for="password" class="sr-only"
                             >Mot de passe</label
